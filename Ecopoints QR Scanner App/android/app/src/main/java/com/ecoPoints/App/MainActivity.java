c069ed825aa5b98dbcb4ac204458c4131dbdddb5
black250306 <lamadridv44@gmail.com>
2025-11-17 07:47:14 -0500

    f

--- Ecopoints QR Scanner App/android/app/src/main/java/com/ecoPoints/App/MainActivity.java
+++
@@ -1,9 +0,0 @@
-<<<<<<<< Updated upstream:Ecopoints QR Scanner App/android/app/src/main/java/com/ecoPoints/App/MainActivity.java
-package com.ecoPoints.App;
-========
-package com.ecopoints.qrscanner;
->>>>>>>> Stashed changes:Ecopoints QR Scanner App/android/app/src/main/java/com/ecopoints/qrscanner/MainActivity.java
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
