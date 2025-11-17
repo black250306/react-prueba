8cfdfdd7870671c4e6828d41a321de8eb2613a4a
lamadridv44 <lamadridv44@gmail.com>
2025-11-17 14:13:51 +0000

    ddd

--- Ecopoints QR Scanner App/src/components/QRScanner.tsx
+++ Ecopoints QR Scanner App/src/components/QRScanner.tsx
@@ -173,11 +173,11 @@ export function QRScanner({ onScanSuccess }: QRScannerProps) {
       // MANTENIENDO TU LÓGICA ORIGINAL para cámara trasera
       let selectedCamera = cameras.find(c => {
         const label = c.label.toLowerCase();
-        return
-        label.includes('facing back') ||
+        return label.includes('facing back') ||
           label.includes('back') ||
           label.includes('rear') ||
           label.includes('trasera') ||
+
           label.includes('0');
       }) || cameras[0];
 
